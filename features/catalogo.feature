54a1481c43820aa3db3fb83985c5d5fc49a001d5
damiaoregis <damiao.v.serrana@gmail.com>
2026-04-03 14:55:08 -0300

    Refatoração do step  e o Cenário: Café indisponível

--- features/catalogo.feature
+++ features/catalogo.feature
@@ -7,19 +7,20 @@ Funcionalidade: Catálogo de cafés
 Cenário: Acessar o catálogo de cafés na página principal
     Quando eu acessar a página principal da Starbugs
     Então eu devo ver uma lista de cafés disponíveis
-@temp
+
 Cenário: Iniciar o processo de compra de um café
     Dado que eu estou na página principal da Starbugs
     E que desejo comprar o seguinte produto:
-      | product  | Expresso Gelado |         
+      | name     | Expresso Gelado |         
       | price    | R$ 9,99         |
       | delivery | R$ 10,00        |
     Quando inicio a compra desse item
     Então devo ver a página de checkout com detahehes do produto
     E o valor total da compra deve ser "R$ 19,99"
-
+@temp
 Cenário: Café indisponível
     Dado que eu estou na página principal da Starbugs
-    E que desejo comprar o café "Expresso Cremoso"
+     E que desejo comprar o seguinte produto:
+      | name     | Expresso Cremoso |
     Quando inicio a compra desse item
     Então devo ver um popup indicando que o produto está indisponível
--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -12,25 +12,23 @@ Dado('que eu estou na página principal da Starbugs') do
 end
 
 Dado('que desejo comprar o seguinte produto:') do |table|
-  @product_name = table.rows_hash[:product]
-  @product_price = table.rows_hash[:price]
-  @delivery_price = table.rows_hash[:delivery]   
+  @product = table.rows_hash
 end
 
 Quando('inicio a compra desse item') do
-   product = find('.coffee-item', text: @product_name)
+   product = find('.coffee-item', text: @product[:name])
    product.find('.buy-coffee').click
 end
 
 Então('devo ver a página de checkout com detahehes do produto') do
   product_title = find('.item-details h1')
-  expect(product_title.text).to eql @product_name
+  expect(product_title.text).to eql @product[:name]
 
   subprice = find('.subtotal .sub-price')
-  expect(subprice.text).to eql @product_price
+  expect(subprice.text).to eql @product[:price]
 
   delivery = find('.delivery-price')
-  expect(delivery.text).to eql @delivery_price
+  expect(delivery.text).to eql @product[:delivery]
 end
 
 Então('o valor total da compra deve ser {string}') do |total_price|
